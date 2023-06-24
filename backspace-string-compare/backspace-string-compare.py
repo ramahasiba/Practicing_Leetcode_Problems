d81f18a0cfd0d0f77e7ce7faa106267d6d7c49d7
Rama Hasiba <ramahaseeba@gmail.com>
2023-06-25 00:36:29 +0300

    Time: 53 ms (21.93%) | Memory: 16.3 MB (34.00%) - LeetSync

--- backspace-string-compare/backspace-string-compare.py
+++ backspace-string-compare/backspace-string-compare.py
@@ -14,24 +14,6 @@ class Solution:
         equals = False
         s = remove_backspace(s)
         t = remove_backspace(t)
-        # s_counter = 0
-        # s_length = len(s)
-        # for index, char in enumerate(s):
-        #     if index < s_length - 1:
-        #         if s[index + 1 - s_counter] == '#':
-        #             s = s[: index - s_counter] + s[index + 2 - s_counter: ]
-        #             s_counter = s_counter + 2   
-
-        # t_counter = 0
-        # t_length = len(t)
-        # for index, char in enumerate(t):
-        #     if index < t_length - 1:
-        #         if t[index + 1 - t_counter] == '#':
-        #             t = t[: index - t_counter] + t[index + 2 - t_counter: ]
-        #             t_counter = t_counter + 2 
-
         if t == s:
             equals = True
-
-
         return equals
