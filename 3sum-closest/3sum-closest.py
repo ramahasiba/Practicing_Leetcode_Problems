836e811970da53ce57540a33c1017d2e9f5abec2
Rama Hasiba <ramahaseeba@gmail.com>
2023-06-25 03:32:20 +0300

    Time: 2899 ms (5.02%) | Memory: 33.1 MB (11.93%) - LeetSync

--- 3sum-closest/3sum-closest.py
+++ 3sum-closest/3sum-closest.py
@@ -4,26 +4,7 @@ class Solution:
         diff = 92233720368547
         nums_length = len(nums)
         result_ = sum(nums[: 3])
-        nums.sort()
-        # first_ptr = 0
-        # sec_ptr = nums_length - 1 
-        # for index in range(nums_length):
-        #     for sec_index in range(index + 1, nums_length):
-        #         for third_index in range(sec_index + 1, nums_length):
-        #             three_sum = sum([nums[index], nums[sec_index], nums[third_index]])
-        #             current_diff = np.abs(three_sum - target) 
-        #             if current_diff == 0:
-        #                 return three_sum
-
-        #             if current_diff < diff:
-        #                 diff = current_diff
-        #                 result_ = three_sum
-# -4, -1, 1, 2     ->       1
-# 5, 2, 2, 
-        # triplets = []
-        # nums.sort()
-        # nums_length = len(nums)
-
+        nums.sort() 
         for index, val in enumerate(nums):
             #skip repeated numbers
             if index > 0 and val == nums[index - 1]:
@@ -34,9 +15,11 @@ class Solution:
             while first_ptr < second_ptr:
                 three_sum = val + nums[first_ptr] + nums[second_ptr]
                 curent_diff = np.abs(target - three_sum)
+
                 if curent_diff < diff:
                     result_ = three_sum
                     diff = curent_diff
+
                 if three_sum > target:
                     second_ptr = second_ptr - 1
 
@@ -45,5 +28,4 @@ class Solution:
 
                 else:
                     return three_sum
-                
         return result_
