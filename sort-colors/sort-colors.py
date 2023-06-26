53e7f1cbbe42575043a67445ba3c091159281e41
Rama Hasiba <ramahaseeba@gmail.com>
2023-06-26 13:53:17 +0300

    Time: 54 ms (31.85%) | Memory: 16.4 MB (31.33%) - LeetSync

--- sort-colors/sort-colors.py
+++ sort-colors/sort-colors.py
@@ -1,13 +1,12 @@
+from collections import Counter  
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums_length = len(nums) 
-        zeros_count = nums.count(0)
-        ones_count = nums.count(1)
-        twos_count = nums.count(2)  
-        nums[: zeros_count] = [0 for _ in range(zeros_count)]
-        ones_index = ones_count + zeros_count
-        nums[zeros_count: ones_index] = [1 for _ in range(ones_count)]
-        nums[ones_index: ] = [2 for _ in range(twos_count)]
+        nums_counter = Counter(nums) 
+        zeros_count = nums_counter[0] 
+        ones_index = nums_counter[1] + nums_counter[0]
+        nums[: nums[0]] = [0 for _ in range(nums_counter[0])]
+        nums[zeros_count: ones_index] = [1 for _ in range(nums_counter[1])]
+        nums[ones_index: ] = [2 for _ in range(nums_counter[2])]
