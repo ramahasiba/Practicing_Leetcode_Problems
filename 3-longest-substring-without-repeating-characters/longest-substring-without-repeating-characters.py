2eb3a7d006a6c80bc59b3ebf816fb919b05cdd21
Rama Hasiba <ramahaseeba@gmail.com>
2023-07-16 11:53:41 +0300

    Time: 356 ms (15.25%) | Memory: 16.4 MB (48.61%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -11,32 +11,33 @@ class Solution:
         else:
             first_ptr, sec_ptr = 0, 1
 
-            longest_substring_length = 1
-            current_substring_length = 1 
+            longest_substring_length = current_substring_length = 1 
 
             substring_chars = {
                 s[0]: 0
             }
             
             while sec_ptr < s_length: 
-                if s[sec_ptr] not in substring_chars:
-                    substring_chars[s[sec_ptr]] = sec_ptr
+                current_char = s[sec_ptr]
+                if current_char not in substring_chars:
+                    substring_chars[current_char] = sec_ptr
                     sec_ptr += 1 
+                    current_substring_length += 1
                 else:
-                    current_substring_length = len(substring_chars)
+                    # current_substring_length = len(substring_chars)
 
                     if current_substring_length > longest_substring_length:
                         longest_substring_length = current_substring_length 
                     
                     substring_chars = {
-                        key: value for key, value in substring_chars.items() if value > substring_chars[s[sec_ptr]]
+                        key: value for key, value in substring_chars.items() if value > substring_chars[current_char]
                     }
 
-                    substring_chars[s[sec_ptr]] = sec_ptr
- 
+                    substring_chars[current_char] = sec_ptr
+                    current_substring_length = len(substring_chars)
                     sec_ptr += 1  
                     
-            current_substring_length = len(substring_chars)
+            # current_substring_length = len(substring_chars)
             if current_substring_length > longest_substring_length:
                 longest_substring_length = current_substring_length 
              
