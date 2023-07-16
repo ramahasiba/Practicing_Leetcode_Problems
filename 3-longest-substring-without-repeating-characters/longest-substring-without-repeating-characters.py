f31724bb3a1fbb67d30d46f05f9c4e5942b57b57
Rama Hasiba <ramahaseeba@gmail.com>
2023-07-16 10:27:23 +0300

    Time: 391 ms (14.71%) | Memory: 16.4 MB (48.61%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -0,0 +1,64 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        save strin characters in a dictionary with thier positions then check if the substring is greater than the current one then replace it 
+        """
+        #get the length of s
+        s_length = len(s)
+
+        #check if there is string
+        if s_length == 0:
+            return 0
+        elif s_length == 1:
+            return 1
+        else:
+            first_ptr, sec_ptr = 0, 1
+
+            longest_substring_length = 1
+            current_substring_length = 1 
+
+            substring_chars = {
+                s[0]: 0
+            }
+            
+            while sec_ptr < s_length: 
+                if s[sec_ptr] not in substring_chars:
+                    substring_chars[s[sec_ptr]] = sec_ptr
+                    sec_ptr += 1 
+                else:
+                    current_substring_length = len(substring_chars)
+
+                    if current_substring_length > longest_substring_length:
+                        longest_substring_length = current_substring_length 
+                    
+                    substring_chars = {
+                        key: value for key, value in substring_chars.items() if value > substring_chars[s[sec_ptr]]
+                    }
+
+                    substring_chars[s[sec_ptr]] = sec_ptr
+
+
+
+                    sec_ptr += 1  
+                    
+            current_substring_length = len(substring_chars)
+            if current_substring_length > longest_substring_length:
+                longest_substring_length = current_substring_length 
+            
+            
+        #     for index, char in enumerate(s): 
+        #         if char not in current_substring:
+        #             current_substring += char
+        #             current_substring_length += 1
+        #         else:
+        #             if current_substring_length > longest_substring_length:
+        #                 longest_substring_length = current_substring_length
+
+        #             slicing_position = current_substring.find(char)
+        #             current_substring = current_substring[slicing_position + 1: current_substring_length + 1]
+        #             current_substring_length = 0
+
+        # if current_substring_length > longest_substring_length:
+        #     longest_substring_length = current_substring_length    
+
+        return longest_substring_length
