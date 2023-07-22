15d8f493d0521f8957e31da2eaff1cb1ed38fc3f
Rama Hasiba <ramahaseeba@gmail.com>
2023-07-22 19:40:01 +0300

    Time: 80 ms (45.11%) | Memory: 16.4 MB (5.55%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        else:
+            x = str(x)
+            x_length = len(x)
+
+            l, r = 0, x_length - 1
+            while l <= r:
+                if x[l] == x[r]:
+                    r -= 1
+                    l += 1
+                else:
+                    return False
+            return True
