9dddc0c8ab3fc2d4e10a50f4c707990d81422a80
Rama Hasiba <ramahaseeba@gmail.com>
2023-07-26 22:26:36 +0300

    Time: 53 ms (42.84%) | Memory: 16.3 MB (36.61%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,20 +1,22 @@
 class Solution:
-    def isHappy(self, n: int) -> bool:
-        squared_sums = set() 
-        while n not in squared_sums:
-            squared_sums.add(n)
-            
-            updated_n = 0
+    def sum_squares(self, n):         
+        updated_n = 0
 
-            while n:
-                digit = n % 10
-                digit = digit ** 2
-                updated_n += digit
-                n = n // 10
+        while n:
+            digit = n % 10
+            digit = digit ** 2
+            updated_n += digit
+            n = n // 10
 
-            n = updated_n
+        n = updated_n 
 
-            if n == 1:
-                return True
+        return n
+        
+    def isHappy(self, n: int) -> bool: 
+        slow = self.sum_squares(n)
+        fast = self.sum_squares(self.sum_squares(n))
 
-        return False
+        while slow!=fast and fast!=1:
+            slow = self.sum_squares(slow)
+            fast = self.sum_squares(self.sum_squares(fast))
+        return fast==1   
