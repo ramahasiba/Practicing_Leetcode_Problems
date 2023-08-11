bc341d11cd6004ba487e136ce8245eed09d5246b
Rama Hasiba <ramahaseeba@gmail.com>
2023-08-11 13:31:55 +0300

    Time: 30 ms (63.95%) | Memory: 17.9 MB (55.82%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -1,5 +1,5 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
@@ -10,7 +10,12 @@ def dfs(lower, upper, root):
     elif root.val <= lower or root.val >= upper:
         return False
     else:
-        return dfs(lower, root.val, root.left) and dfs(root.val, upper, root.right)
-class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return dfs(lower, root.val, root.left) and dfs(root.val, upper, root.right) 
+        
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
         return dfs(float('-inf'), float('inf'), root)
