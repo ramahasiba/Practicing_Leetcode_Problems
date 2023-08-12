f5cf7fafd766b2859a707342dc0a83338d14ac9a
Rama Hasiba <ramahaseeba@gmail.com>
2023-08-12 16:31:03 +0300

    Time: 58 ms (57.17%) | Memory: 18.6 MB (73.17%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -3,25 +3,31 @@
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
-#         self.right = right
-def maxDepth(root: Optional[TreeNode]) -> int: 
-        return max(
-            (maxDepth(root.left),
-            maxDepth(root.right))
-        ) +1 if root else 0
-
+#         self.right = right 
 class Solution:
-    diameter = 0
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+        diameter = [0] 
+
+        def dfs(root):
+            if not root:
+                return -1
             
-        self.diameterOfBinaryTree(root.right)
-        self.diameterOfBinaryTree(root.left)
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            diameter[0] = max(diameter[0], (2 + left + right))
+
+            return 1 + max(left, right)
+        
+        dfs(root)
+        return diameter[0]
+
+        # self.diameterOfBinaryTree(root.right)
+        # self.diameterOfBinaryTree(root.left)
           
-        right_max_depth = maxDepth(root.right) 
-        left_max_depth = maxDepth(root.left)
+        # right_max_depth = maxDepth(root.right) 
+        # left_max_depth = maxDepth(root.left)
 
-        self.diameter = max(self.diameter, (right_max_depth + left_max_depth))
+        # self.diameter = max(self.diameter, (right_max_depth + left_max_depth))
 
-        return self.diameter 
+        # return self.diameter 
