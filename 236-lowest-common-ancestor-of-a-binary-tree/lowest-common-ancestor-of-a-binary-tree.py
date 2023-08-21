dbcb46e4861be00dac2606f30bd39df442766c51
Rama Hasiba <ramahaseeba@gmail.com>
2023-08-21 21:18:25 +0300

    Time: 77 ms (56.30%) | Memory: 28.7 MB (40.19%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -3,30 +3,13 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
-#         self.right = None
-def findPath(root: TreeNode, target: int) -> List[int]:
-    path = []
-    dfs(root, target, path)
-    return path
-def dfs(node, target, path):
-    if not node:
-        return False 
-    # Add the current node's value to the path
-    path.append(node.val) 
-    if node.val == target:
-        return True 
-    if dfs(node.left, target, path) or dfs(node.right, target, path):
-        return True 
-    # Remove the current node's value from the path if the target is not found
-    path.pop()
-    return False 
-
+#         self.right = None 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode': 
-        p_path = findPath(root, p.val)
-        q_path = findPath(root, q.val) 
-        intersection = [x for x in p_path if x in q_path]
-        if intersection:
-            return TreeNode(intersection[-1])
-        else:
-            return root 
+        if not root or root.val == p.val or root.val == q.val:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q) 
+        if left and right:
+            return root
+        return left or right 
