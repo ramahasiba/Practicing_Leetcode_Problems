56722a0cca0d4c56611a2830bd3a248ca6dc2f72
Rama Hasiba <ramahaseeba@gmail.com>
2023-09-13 23:16:52 +0300

    Time: 70 ms (38.95%) | Memory: 24.7 MB (11.44%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -9,14 +9,16 @@ class Solution(object):
         """
         nums1_len = len(nums1)
         nums2_len = len(nums2)
+        merged_len = nums1_len + nums2_len
 
         def add_zeros():
             for _ in range(nums2_len):
                 nums1.append(0)
+
         add_zeros() 
 
         def merge_arrays(nums2_len, nums1_len):
-            write_index = nums1_len + nums2_len + 1
+            write_index = merged_len - 1
             while nums2_len >= 0:
                 if nums1_len >= 0 and nums1[nums1_len] > nums2[nums2_len]:
                     nums1[write_index] = nums1[nums1_len]
@@ -27,9 +29,15 @@ class Solution(object):
 
                 write_index -= 1
 
-        merge_arrays(nums2_len - 1, nums1_len - 1)
- 
-        return np.median(nums1)
+        merge_arrays(nums2_len - 1, nums1_len - 1) 
+        if merged_len % 2 == 0:
+            return (nums1[merged_len//2] + nums1[(merged_len // 2) - 1 ]) / 2.0
+
+        return nums1[merged_len//2]  
+        
+            
+        
+        
         
 
 
