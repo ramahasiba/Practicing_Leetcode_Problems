104cef42424cb27902e8a77c77352f5cf1d8122c
Rama Hasiba <ramahaseeba@gmail.com>
2024-02-04 01:01:48 +0300

    Time: 45 ms (77.08%) | Memory: 16.6 MB (67.13%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,23 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        symbol_values = {
+            1: 'I', 
+            5: 'V', 
+            10:'X',   
+            50: 'L', 
+            100:'C', 
+            500: 'D', 
+            1000: 'M', 
+            4: 'IV', 
+            9: 'IX',
+            40: 'XL',
+            90: 'XC',
+            400: 'CD',
+            900: 'CM'    
+        } 
+        roman=''
+        for key, val in sorted(symbol_values.items(), reverse=True): 
+            while num >= key:
+                roman+=val
+                num-=key 
+        return roman   
