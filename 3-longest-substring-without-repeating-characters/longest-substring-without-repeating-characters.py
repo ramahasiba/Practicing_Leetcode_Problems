c9ef4bf19a8354a80665111940ce60a2aeead100
Rama Hasiba <ramahaseeba@gmail.com>
2024-07-19 23:01:57 +0300

    Time: 51 ms (76.85%) | Memory: 16.8 MB (10.30%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,43 +1,13 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int: 
-        #get the length of s
-        s_length = len(s)
-
-        #check if there is string
-        if s_length == 0:
-            return 0
-        elif s_length == 1:
-            return 1
-        else:
-            first_ptr, sec_ptr = 0, 1
-
-            longest_substring_length = current_substring_length = 1 
-
-            substring_chars = {
-                s[0]: 0
-            }
-            
-            while sec_ptr < s_length: 
-                current_char = s[sec_ptr]
-                if current_char not in substring_chars:
-                    substring_chars[current_char] = sec_ptr
-                    sec_ptr += 1 
-                    current_substring_length += 1
-                else:
-                    # current_substring_length = len(substring_chars)
-
-                    if current_substring_length > longest_substring_length:
-                        longest_substring_length = current_substring_length 
-                    
-                    substring_chars = {
-                        key: value for key, value in substring_chars.items() if value > substring_chars[current_char]
-                    }
-
-                    substring_chars[current_char] = sec_ptr
-                    current_substring_length = len(substring_chars)
-                    sec_ptr += 1  
-                     
-            if current_substring_length > longest_substring_length:
-                longest_substring_length = current_substring_length 
-             
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_index_map = {}
+        longest_substring_length = 0
+        start = 0
+        
+        for end, char in enumerate(s):
+            if char in char_index_map and char_index_map[char] >= start:
+                start = char_index_map[char] + 1
+            char_index_map[char] = end
+            longest_substring_length = max(longest_substring_length, end - start + 1)
+        
         return longest_substring_length
