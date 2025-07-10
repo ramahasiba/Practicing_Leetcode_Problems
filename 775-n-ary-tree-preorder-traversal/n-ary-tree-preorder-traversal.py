310df281a3f0bc7fd45aced0bb6d22aeaeb80100
Rama Hasiba <ramahaseeba@gmail.com>
2025-07-10 20:46:44 +0300

    Time: 45 ms (78.92%) | Memory: 19.3 MB (76.44%) - LeetSync

---
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        result = []
+        def traverse(tree):
+            if tree:
+                result.append(tree.val)
+                for child in tree.children:
+                    traverse(child)
+        traverse(root)
+        return result
+        
